987521b0b82d51d14584d611e56133985fd58a90
Tavisco <git@tavisco.dev>
2024-08-22 21:02:37 -0300

    Only store last 1000 log entries

--- src/log-store.ts
+++ src/log-store.ts
@@ -1,5 +1,5 @@
-import {action, makeObservable, observable} from 'mobx';
-import {debug} from 'palm-sync';
+import { action, makeObservable, observable } from 'mobx';
+import { debug } from 'palm-sync';
 
 export enum LogEntryType {
   LOG = 'log',
@@ -17,6 +17,8 @@ export type LogEntry =
     };
 
 class LogStore {
+  private static readonly MAX_LOG_ENTRIES = 1000;
+
   constructor() {
     makeObservable(this, {
       logs: observable,
@@ -28,6 +30,13 @@ class LogStore {
   }
 
   readonly logs: Array<LogEntry> = [];
+
+  private maintainLogSize() {
+    // Remove one item from the front if the array exceeds the max size
+    if (this.logs.length > LogStore.MAX_LOG_ENTRIES) {
+      this.logs.shift();
+    }
+  }
 
   addLog(message: string) {
     // eslint-disable-next-line prefer-rest-params
@@ -44,17 +53,20 @@ class LogStore {
       return;
     }
 
-    console.log(...arguments);
+    // console.log(...arguments);
     this.logs.push({
       type: LogEntryType.LOG,
       module,
       message,
     });
+
+    this.maintainLogSize();
   }
 
   addDivider() {
     console.log('-'.repeat(40));
-    this.logs.push({type: LogEntryType.DIVIDER});
+    this.logs.push({ type: LogEntryType.DIVIDER });
+    this.maintainLogSize();
   }
 }
 
