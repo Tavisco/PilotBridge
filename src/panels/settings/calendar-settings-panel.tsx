cd49acd27a718ac750a9203b4305aedc0cdfb5e9
Tavisco <git@tavisco.dev>
2024-11-18 20:20:05 -0300

    Basic iCal settings done

--- src/panels/settings/calendar-settings-panel.tsx
+++ src/panels/settings/calendar-settings-panel.tsx
@@ -1,20 +1,37 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
 import SaveIcon from '@mui/icons-material/Save';
+import { prefsStore } from '../../prefs-store';
+import { useEffect, useState } from "react";
 
 export function CalendarSettingsPanel() {
+
+  const [currentICalendarURL, setCurrentICalendarURL] = useState('');
+  const [saveButtonColor, setSaveButtonColor] = useState('primary');
+
+  useEffect(() => {
+    setCurrentICalendarURL(prefsStore.get('iCalendarURL'));
+  }, []);
+
+  const handleSaveClick = () => {
+    setSaveButtonColor('success');
+    prefsStore.set('iCalendarURL', currentICalendarURL);
+  }
 
   return (
     <Box
       component="form"
-      sx={{ '& > :not(style)': { m: 1} }}
+      sx={{ '& > :not(style)': { m: 1 } }}
       noValidate
       autoComplete="off"
     >
       <Typography variant="body1">
-      Enter the iCalendar URL:
+        Enter the iCalendar URL:
       </Typography>
-      <TextField id="outlined-basic" label="iCalendar URL" variant="outlined" />
-      <Button variant="contained" endIcon={<SaveIcon />}  size="large" sx={{height: '6ch', width: '12ch'}}>
+      <TextField id="outlined-basic" label="iCalendar URL" variant="outlined" value={currentICalendarURL}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setCurrentICalendarURL(event.target.value);
+        }} />
+      <Button variant="contained" endIcon={<SaveIcon />} size="large" sx={{ height: '6ch', width: '12ch' }} color={saveButtonColor as any} onClick={handleSaveClick}>
         Save
       </Button>
     </Box>
