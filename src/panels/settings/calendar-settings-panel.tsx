2a98c3014509a1787efc76d3a74046031e5cedd6
Tavisco <git@tavisco.dev>
2024-11-18 19:49:38 -0300

    Add text box for iCal URL

--- src/panels/settings/calendar-settings-panel.tsx
+++ src/panels/settings/calendar-settings-panel.tsx
@@ -1,10 +1,22 @@
-import { Box } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import SaveIcon from '@mui/icons-material/Save';
 
 export function CalendarSettingsPanel() {
 
   return (
-    <Box>
-        
+    <Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1} }}
+      noValidate
+      autoComplete="off"
+    >
+      <Typography variant="body1">
+      Enter the iCalendar URL:
+      </Typography>
+      <TextField id="outlined-basic" label="iCalendar URL" variant="outlined" />
+      <Button variant="contained" endIcon={<SaveIcon />}  size="large" sx={{height: '6ch', width: '12ch'}}>
+        Save
+      </Button>
     </Box>
   );
 }
